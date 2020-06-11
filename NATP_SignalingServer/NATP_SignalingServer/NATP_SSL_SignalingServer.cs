4b8b568c3af916d64a2fd8fab8283ae41665e266
cxz456999 <cxz456999@gmail.com>
2020-06-11 16:33:29 +0800

    Add Mirro Transport package

---
+++ NATP_SignalingServer/NATP_SignalingServer/NATP_SSL_SignalingServer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using NetCoreServer;
+
+namespace NATP.Signaling.Server
+{
+    class NATP_SSL_SignalingSession : SslSession, INATP_SignalingServerSender
+    {
+        private NATP_SignalingServerCore sigCore;
+        public NATP_SSL_SignalingSession(SslServer server) : base(server) { sigCore = new NATP_SignalingServerCore(this); }
+        protected override void OnConnected()
+        {
+            NATP_OnConnected();
+        }
+
+        protected override void OnHandshaked()
+        {
+            Console.WriteLine($"Chat SSL session with Id {Id} handshaked!");
+        }
+
+        protected override void OnDisconnected()
+        {
+            Console.WriteLine($"Chat SSL session with Id {Id} disconnected!");
+            sigCore.OnDisconnected();
+        }
+
+        protected override void OnReceived(byte[] buffer, long offset, long size)
+        {
+            sigCore.OnResponse(buffer, offset, size);
+        }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"Chat SSL session caught an error with code {error}");
+        }
+
+
+        public void NATP_OnConnected()
+        {
+            Console.WriteLine("IP " + IPAddress.Parse(((IPEndPoint)Socket.RemoteEndPoint).Address.ToString()) + " on port number " + ((IPEndPoint)Socket.RemoteEndPoint).Port.ToString() + " connected!");
+            sigCore.RemoteEndPoint = (IPEndPoint)Socket.RemoteEndPoint;
+        }
+    }
+
+    class NATP_SSL_SignalingServer : SslServer
+    {
+        public NATP_SSL_SignalingServer(SslContext context, IPAddress address, int port) : base(context, address, port) { }
+
+        protected override SslSession CreateSession() { return new NATP_SSL_SignalingSession(this); }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"Chat SSL server caught an error with code {error}");
+        }
+    }
+}
---
+++ NATP_SignalingServer/NATP_SignalingServer/NATP_TCP_SignalingServer.cs
@@ -0,0 +1,51 @@
+﻿using NetCoreServer;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NATP.Signaling.Server
+{
+    public class NATP_TCP_SignalingSession : TcpSession, INATP_SignalingServerSender
+    {
+        private NATP_SignalingServerCore sigCore;
+        public NATP_TCP_SignalingSession(TcpServer server) : base(server) { sigCore = new NATP_SignalingServerCore(this); }
+
+        protected override void OnConnected()
+        {
+            NATP_OnConnected();
+        }
+
+        protected override void OnDisconnected()
+        {
+            Console.WriteLine($"Chat TCP session with Id {Id} disconnected!");
+            sigCore.OnDisconnected();
+        }
+
+        protected override void OnReceived(byte[] buffer, long offset, long size)
+        {
+            sigCore.OnResponse(buffer, offset, size);
+        }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"Chat TCP session caught an error with code {error}");
+        }
+
+        public void NATP_OnConnected()
+        {
+            Console.WriteLine("IP " + IPAddress.Parse(((IPEndPoint)Socket.RemoteEndPoint).Address.ToString()) + " on port number " + ((IPEndPoint)Socket.RemoteEndPoint).Port.ToString() + " connected!");
+            sigCore.RemoteEndPoint = (IPEndPoint)Socket.RemoteEndPoint;
+        }
+    }
+    public class NATP_TCP_SignalingServer : TcpServer
+    {
+        public NATP_TCP_SignalingServer(IPAddress address, int port) : base(address, port) { }
+
+        protected override TcpSession CreateSession() { return new NATP_TCP_SignalingSession(this); }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"Chat TCP server caught an error with code {error}");
+        }
+    }
+}
